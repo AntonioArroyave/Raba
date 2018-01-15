55db30230a66a0f5dd0ac42fc7cc65341bc93d4a
Rafael Barrientos <rafaelbarrientosarroyave@gmail.com>
2018-01-15 09:09:39 -0500

    Primera version del script de configuración para windows

---
+++ raba.bat
@@ -0,0 +1,18 @@
+@echo off
+::Script para configurar la terminal  CMD de RABA en windows.
+::Verificar si los programas estan instalados
+
+::Mapeo de comandos comunes de Windows a Linux
+doskey ls=dir /B
+doskey alias=vim raba.bat
+doskey clear=cls
+doskey rm=del $* 
+doskey mv=move $*
+::Comandos propios del pograma raba
+doskey rabaedit=vim C:\Users\0075\Documents\Scripts\raba\raba.bat
+::Rutas de acceso rapido
+doskey Documentos=cd C:\Users\0075\Documents
+doskey configvim=cd C:\"Program Files (x86)"\Vim
+doskey Programas=cd C:\Program Files\
+doskey Programas86=cd C:\"Program Files (x86)"
+ 
