be125e3dc63eff4ba11335610632ca83a62f4fc0
Antonio Arroyave <rafaelbarrientosarroyave@gmail.com>
2018-01-16 21:05:03 -0500

    Añadida la ruta al repositorio

--- raba.bat
+++ raba.bat
@@ -8,12 +8,25 @@ doskey alias=vim raba.bat
 doskey clear=cls
 doskey rm=del $* 
 doskey mv=move $*
+doskey cat=type $*
+doskey man=help $*
+doskey cp=copy $*
+doskey pwd=chdir
 ::Comandos propios del pograma raba
 doskey rabasave=echo $* >> C:\Users\0075\Documents\Scripts\raba\raba.bat 
-doskey rabaedit=vim C:\Users\0075\Documents\Scripts\raba\raba.bat
+doskey rabaedit=vim C:\Users\Lenovo\Raba\raba.bat
+doskey cdraba=cd C:\Users\Lenovo\Raba
+doskey rabainsert=echo $* >> C:\Users\Lenovo\Raba\raba.bat
 ::Rutas de acceso rapido
 doskey Documentos=cd C:\Users\0075\Documents
 doskey configvim=cd C:\"Program Files (x86)"\Vim
 doskey Programas=cd C:\Program Files\
 doskey Programas86=cd C:\"Program Files (x86)"
+::Estilo
+title Raba Console
+prompt Raba $G
+::Ejecucion de pogramas por defecto
+doskey firefox=C:\Program Files\Mozilla Firefox\firefox.exe
+
+
  
