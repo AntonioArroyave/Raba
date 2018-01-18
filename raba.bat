2fc58d1de6a6adf683c97ff5fbabe3b781310f97
Antonio Arroyave <rafaelbarrientosarroyave@gmail.com>
2018-01-17 21:21:28 -0500

    rabapath 2.0

--- raba.bat
+++ raba.bat
@@ -1,6 +1,6 @@
 @echo off
 ::Script para configurar la terminal  CMD de RABA en windows.
-set rabapath=chdir
+set rabapath=C:\Users\Lenovo\Raba\
 ::Verificar si los programas estan instalados
 
 ::Mapeo de comandos comunes de Windows a Linux
@@ -14,12 +14,12 @@ doskey man=help $*
 doskey cp=copy $*
 doskey pwd=chdir
 ::Comandos propios del pograma raba
-doskey rabasave=echo $* >> C:\Users\0075\Documents\Scripts\raba\raba.bat 
-doskey rabaedit=vim C:\Users\Lenovo\Raba\raba.bat
-doskey cdraba=cd C:\Users\Lenovo\Raba
-doskey rabainsert=echo $* >> C:\Users\Lenovo\Raba\raba.bat
+doskey rabasave=echo $* >> %rabapath%raba.bat 
+doskey rabaedit=vim  %rabapath%raba.bat
+doskey cdraba=cd %rabapath%
+doskey rabainsert=echo $* >> %rabapath%raba.bat
 ::Rutas de acceso rapido
-doskey Documentos=cd C:\Users\0075\Documents
+doskey Documentos=cd C:\Users\Lenovo\Documents
 doskey configvim=cd C:\"Program Files (x86)"\Vim
 doskey Programas=cd C:\Program Files\
 doskey Programas86=cd C:\"Program Files (x86)"
@@ -27,7 +27,6 @@ doskey Programas86=cd C:\"Program Files (x86)"
 title Raba Console
 prompt Raba $G
 ::Atajos de pogramas por defecto
-doskey firefox=C:\"Program Files"\Mozilla Firefox"\firefox.exe
 ::Correr los Autohoykey scripts
 start /B C:\Users\Lenovo\Raba\raba.ahk
 
---
+++ rabainstall.bat
@@ -0,0 +1,7 @@
+::la idea es configurar una uri para descargar este archivo, que ejecutara los comandos necesarios para instalar raba
+::Configuracion de la variable rabapath
+rabaregister.reg
+::a travez de un .reg, ademas hay que modificar tambien el registro para tomar automaticamente el rabaconsole
+::Configuracion de rabaconsole
+
+::Configuracion de vim
